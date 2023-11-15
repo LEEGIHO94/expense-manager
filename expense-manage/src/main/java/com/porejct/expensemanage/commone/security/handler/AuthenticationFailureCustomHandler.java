c6f196eefce95cb3ea1d1d561a9357660695c84e
Siglee2247 <lgh941107@gmail.com>
2023-11-16 05:10:24 +0900

    refactor: ResponseUtils 이름 변경 #3 - ResponseUtils -> ErrorResponseUtils 변경 - Error 관련 Body 구현 작업 모두 이관

--- expense-manage/src/main/java/com/porejct/expensemanage/commone/security/handler/AuthenticationFailureCustomHandler.java
+++ expense-manage/src/main/java/com/porejct/expensemanage/commone/security/handler/AuthenticationFailureCustomHandler.java
@@ -1,10 +1,6 @@
 package com.porejct.expensemanage.commone.security.handler;
 
-import com.porejct.expensemanage.commone.exception.ErrorResponse;
-import com.porejct.expensemanage.commone.security.exception.AuthExceptionCode;
-import com.porejct.expensemanage.commone.utils.response.ResponseUtils;
-import com.porejct.expensemanage.commone.utils.translator.ObjectMapperUtils;
-import jakarta.servlet.ServletException;
+import com.porejct.expensemanage.commone.utils.response.ErrorResponseUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,26 +14,16 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class AuthenticationFailureCustomHandler implements AuthenticationFailureHandler {
-    private final ObjectMapperUtils objectMapperUtils;
+
+    private final ErrorResponseUtils errorResponseUtils;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException exception) throws IOException {
         log.error("# Authentication failed: {}", exception.getMessage());
         log.error("authentication ", exception);
-        getSendErrorResponse(response);
+
+        errorResponseUtils.sendErrorResponse(response);
     }
 
-    private void getSendErrorResponse(HttpServletResponse response) throws IOException {
-        new ResponseUtils(response,getResponseData()).sendErrorResponse();
-    }
-
-    private String getResponseData() {
-        return objectMapperUtils.toStringValue(createErrorResponse());
-    }
-
-    private ErrorResponse createErrorResponse() {
-        return ErrorResponse.of(AuthExceptionCode.UNAUTHENTICATED.getHttpStatus(),
-                AuthExceptionCode.UNAUTHENTICATED.getMessage());
-    }
 }
