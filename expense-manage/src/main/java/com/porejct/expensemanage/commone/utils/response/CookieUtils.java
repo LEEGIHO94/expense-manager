299a846d79d5db943b41b07efda51273f651398f
Siglee2247 <lgh941107@gmail.com>
2023-11-16 06:08:50 +0900

    feat: LogoutHanlder 구현 #3

--- expense-manage/src/main/java/com/porejct/expensemanage/commone/utils/response/CookieUtils.java
+++ expense-manage/src/main/java/com/porejct/expensemanage/commone/utils/response/CookieUtils.java
@@ -1,6 +1,9 @@
 package com.porejct.expensemanage.commone.utils.response;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +19,24 @@ public class CookieUtils {
         cookie.setMaxAge(60 * cookieProperties.getLimitTime());
         return cookie;
     }
+
+    public Cookie deleteCookie(HttpServletRequest request) {
+        Cookie cookie = searchCookieProperties(validCookiesExist(request));
+        cookie.setMaxAge(0);
+        return cookie;
+    }
+
+    private Cookie[] validCookiesExist(HttpServletRequest request) {
+        return Optional.ofNullable(request.getCookies()).orElse(new Cookie[]{});
+    }
+
+    private Cookie searchCookieProperties(Cookie[] cookies) {
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("Refresh"))
+                .findFirst()
+                .orElse(new Cookie("Refresh",""));
+    }
+
 
 
 }
