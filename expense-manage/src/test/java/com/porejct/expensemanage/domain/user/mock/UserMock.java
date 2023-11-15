f9716ad01c1300b2f42c28d6170aa0840d6b0d4a
Siglee2247 <lgh941107@gmail.com>
2023-11-16 06:44:19 +0900

    test: 시큐리티 테스트 코드 작성 #3 - 로그인, 로그아웃, 토큰 재발급 코드 작성

---
+++ expense-manage/src/test/java/com/porejct/expensemanage/domain/user/mock/UserMock.java
@@ -0,0 +1,51 @@
+package com.porejct.expensemanage.domain.user.mock;
+
+import com.porejct.expensemanage.commone.security.dto.LoginDto;
+import com.porejct.expensemanage.commone.security.dto.UserInfo;
+import com.porejct.expensemanage.domain.user.entity.User;
+import com.porejct.expensemanage.domain.user.enums.UserRole;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMock {
+
+    private final PasswordEncoder encoder;
+    private final Long userId = 1L;
+    private final String email = "test@gmail.com";
+    private final String rawPassword = "test131!$@2";
+    private final String wrongPassword = "test131!$@2!32!23";
+    private final UserRole userRole = UserRole.USER;
+
+    public UserMock(PasswordEncoder passwordEncoder) {
+        this.encoder = passwordEncoder;
+    }
+
+
+    public User entityMock() {
+        return User.builder()
+                .id(userId)
+                .password(encoder.encode(rawPassword))
+                .userRole(userRole)
+                .email(email)
+                .build();
+    }
+
+    public LoginDto loginMock() {
+        return new LoginDto(email,rawPassword);
+    }
+
+    public LoginDto wrongLoginMock() {
+        return new LoginDto(email,wrongPassword);
+    }
+
+    public UserInfo userInfoMock() {
+        return new UserInfo(email,"ROLE_USER");
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
