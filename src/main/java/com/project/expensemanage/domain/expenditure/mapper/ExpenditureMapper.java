bb4a8e61510298266039adb3cc55b7cb3b0182d6
Siglee2247 <lgh941107@gmail.com>
2023-11-24 04:22:58 +0900

    feat: 지출 단건 조회 service 구현 #15

--- src/main/java/com/project/expensemanage/domain/expenditure/mapper/ExpenditureMapper.java
+++ src/main/java/com/project/expensemanage/domain/expenditure/mapper/ExpenditureMapper.java
@@ -1,10 +1,12 @@
 package com.project.expensemanage.domain.expenditure.mapper;
 
+import com.project.expensemanage.domain.category.dto.GetCategoryResponse;
 import com.project.expensemanage.domain.category.mapper.CategoryMapper;
 import com.project.expensemanage.domain.expenditure.controller.dto.request.PostExpenditureRequest;
 import com.project.expensemanage.domain.expenditure.controller.dto.response.ExpenditureIdResponse;
 import com.project.expensemanage.domain.expenditure.entity.Expenditure;
 import com.project.expensemanage.domain.expenditure.enums.ExcludeSpendingTotal;
+import com.project.expensemanage.domain.expenditure.service.ExpenditureResponse;
 import com.project.expensemanage.domain.user.mapper.UserMapper;
 import com.project.expensemanage.domain.vo.Price;
 import org.springframework.stereotype.Component;
@@ -26,6 +28,16 @@ public class ExpenditureMapper {
     public ExpenditureIdResponse toIdDto(Expenditure entity) {
         return ExpenditureIdResponse.builder()
                 .expenditureId(entity.getId())
+                .build();
+    }
+
+    public ExpenditureResponse toDto(Expenditure entity) {
+        return ExpenditureResponse.builder()
+                .expenditureId(entity.getId())
+                .amount(entity.getPrice().getValue())
+                .expendedDate(entity.getExpendedDate())
+                .memo(entity.getMemo())
+                .category(new GetCategoryResponse(entity.getCategory().getId(),entity.getCategory().getName()))
                 .build();
     }
 }
--- src/main/java/com/project/expensemanage/domain/expenditure/service/ExpenditureService.java
+++ src/main/java/com/project/expensemanage/domain/expenditure/service/ExpenditureService.java
@@ -4,6 +4,7 @@ import com.project.expensemanage.commone.exception.BusinessLogicException;
 import com.project.expensemanage.domain.category.service.CategoryValidService;
 import com.project.expensemanage.domain.expenditure.controller.dto.request.PostExpenditureRequest;
 import com.project.expensemanage.domain.expenditure.controller.dto.response.ExpenditureIdResponse;
+import com.project.expensemanage.domain.expenditure.entity.Expenditure;
 import com.project.expensemanage.domain.expenditure.exception.ExpenditureExceptionCode;
 import com.project.expensemanage.domain.expenditure.mapper.ExpenditureMapper;
 import com.project.expensemanage.domain.expenditure.repoistory.ExpenditureRepository;
@@ -26,12 +27,25 @@ public class ExpenditureService {
         return mapper.toIdDto(repository.save(mapper.toEntity(post, userId)));
     }
 
-    public void deleteExpenditure(Long expenditureId,Long userId) {
+    public void deleteExpenditure(Long expenditureId, Long userId) {
         repository.findById(expenditureId).ifPresent(expenditure -> {
             if (!expenditure.getUser().getId().equals(userId)) {
                 throw new BusinessLogicException(UserExceptionCode.USER_NOT_SAME);
             }
             repository.deleteById(expenditureId);
         });
+    }
+
+    public ExpenditureResponse getExpenditureDetails(Long userId, Long expenditureId) {
+        Expenditure entity = validExpenditure(expenditureId);
+        if (!entity.getUser().getId().equals(userId)) {
+            throw new BusinessLogicException(UserExceptionCode.USER_NOT_SAME);
+        }
+        return mapper.toDto(entity);
+    }
+
+    private Expenditure validExpenditure(Long expenditureId) {
+        return repository.findById(expenditureId)
+                .orElseThrow(() -> new BusinessLogicException(ExpenditureExceptionCode.NOT_FOUND));
     }
 }
