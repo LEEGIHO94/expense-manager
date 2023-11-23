ef42d786c1c86f722a7af81a53b58e2ac61a4572
Siglee2247 <lgh941107@gmail.com>
2023-11-24 03:58:33 +0900

    feat: 지출 삭제를 위한 delete service 구현 #15 - 삭제 시 삭제할 데이터가 존재할 경우 userId가 동일한지 여부만 검증한 뒤 삭제 - 삭제할 데이터가 없을 경우 그대로 반환

--- src/main/java/com/project/expensemanage/domain/expenditure/service/ExpenditureService.java
+++ src/main/java/com/project/expensemanage/domain/expenditure/service/ExpenditureService.java
@@ -1,10 +1,13 @@
 package com.project.expensemanage.domain.expenditure.service;
 
+import com.project.expensemanage.commone.exception.BusinessLogicException;
 import com.project.expensemanage.domain.category.service.CategoryValidService;
 import com.project.expensemanage.domain.expenditure.controller.dto.request.PostExpenditureRequest;
 import com.project.expensemanage.domain.expenditure.controller.dto.response.ExpenditureIdResponse;
+import com.project.expensemanage.domain.expenditure.exception.ExpenditureExceptionCode;
 import com.project.expensemanage.domain.expenditure.mapper.ExpenditureMapper;
 import com.project.expensemanage.domain.expenditure.repoistory.ExpenditureRepository;
+import com.project.expensemanage.domain.user.exception.UserExceptionCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,4 +26,12 @@ public class ExpenditureService {
         return mapper.toIdDto(repository.save(mapper.toEntity(post, userId)));
     }
 
+    public void deleteExpenditure(Long expenditureId,Long userId) {
+        repository.findById(expenditureId).ifPresent(expenditure -> {
+            if (!expenditure.getUser().getId().equals(userId)) {
+                throw new BusinessLogicException(UserExceptionCode.USER_NOT_SAME);
+            }
+            repository.deleteById(expenditureId);
+        });
+    }
 }
--- src/main/java/com/project/expensemanage/domain/user/exception/UserExceptionCode.java
+++ src/main/java/com/project/expensemanage/domain/user/exception/UserExceptionCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum UserExceptionCode implements ExceptionCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 조회 실패"),
-    USER_EXIST(HttpStatus.CONFLICT, "회원이 이미 존재");
+    USER_EXIST(HttpStatus.CONFLICT, "회원이 이미 존재"),
+    USER_NOT_SAME(HttpStatus.BAD_REQUEST, "사용자 정보가 동일하지 않습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
