72fd09763c8906f038f517851b643687f6390b87
Siglee2247 <lgh941107@gmail.com>
2023-11-24 03:59:28 +0900

    feat: 지출 삭제를 위한 delete controller 구현 #15

--- src/main/java/com/project/expensemanage/domain/expenditure/controller/ExpenditureController.java
+++ src/main/java/com/project/expensemanage/domain/expenditure/controller/ExpenditureController.java
@@ -11,6 +11,8 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +36,11 @@ public class ExpenditureController {
 
         URI location = UrlCreator.createUri(DEFAULT, response.getData().expenditureId());
         return ResponseEntity.created(location).body(response);
+    }
+
+    @DeleteMapping("/{expenditureId}")
+    public ResponseEntity<Void> deleteExpenditure(@PathVariable Long expenditureId,@CurrentUser Long userId) {
+        service.deleteExpenditure(expenditureId,userId);
+        return ResponseEntity.noContent().build();
     }
 }
