de5e0a08192b3aa6c0f2ea8ef1601327466cfa98
Siglee2247 <lgh941107@gmail.com>
2023-11-27 17:37:29 +0900

    test: 카테고리 별 조회 db 테스트 이름 수정 #15

--- src/test/java/com/project/expensemanage/domain/budget/repository/BudgetRepositoryTest.java
+++ src/test/java/com/project/expensemanage/domain/budget/repository/BudgetRepositoryTest.java
@@ -1,8 +1,6 @@
 package com.project.expensemanage.domain.budget.repository;
 
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 import com.project.expensemanage.domain.budget.entity.Budget;
 import com.project.expensemanage.domain.budget.repository.dto.RecommendedBudgetData;
 import java.util.HashMap;
@@ -19,20 +17,23 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 class BudgetRepositoryTest {
+
     @Autowired
     BudgetRepository repository;
 
     @Test
-    @DisplayName("Test")
-    void TEst() throws Exception{
+    @DisplayName("DB 카테고리 별 총 금액 조회 테스트 : 성공")
+    void find_amount_by_category_test() throws Exception {
         List<RecommendedBudgetData> result = repository.findTotalAmountByCategory();
 
-        Map<Long,Long> validMap = new HashMap<>();
+        Map<Long, Long> validMap = new HashMap<>();
         List<Budget> allData = repository.findAll();
 
-        allData.forEach(budget -> validMap.put(budget.getCategory().getId(),validMap.getOrDefault(budget.getCategory().getId(),0L) + budget.getPrice().getValue()));
+        allData.forEach(budget -> validMap.put(budget.getCategory().getId(),
+                validMap.getOrDefault(budget.getCategory().getId(), 0L) + budget.getPrice()
+                        .getValue()));
 
-        result.forEach(data -> Assertions.assertThat(data.getAmount()).isEqualTo(validMap.get(data.getCategoryId())));
-
+        result.forEach(data -> Assertions.assertThat(data.getAmount())
+                .isEqualTo(validMap.get(data.getCategoryId())));
     }
 }
