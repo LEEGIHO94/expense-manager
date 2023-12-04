0db3ae28ed6627d5823f56a54d91c39a1d175615
Siglee2247 <lgh941107@gmail.com>
2023-12-04 19:31:03 +0900

    refactoer: 날짜 관련 메서드 유틸클래스 분리 #15

---
+++ src/main/java/com/project/expensemanage/commone/utils/date/DateUtils.java
@@ -0,0 +1,16 @@
+package com.project.expensemanage.commone.utils.date;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DateUtils {
+  public LocalDate startOfMonth() {
+    return YearMonth.now().atDay(1);
+  }
+
+  public LocalDate endOfMonth() {
+    return YearMonth.now().atEndOfMonth();
+  }
+}
--- src/main/java/com/project/expensemanage/recommendation/event/publisher/DailyExpenditureRecommendationPublisher.java
+++ src/main/java/com/project/expensemanage/recommendation/event/publisher/DailyExpenditureRecommendationPublisher.java
@@ -1,13 +1,12 @@
 package com.project.expensemanage.recommendation.event.publisher;
 
+import com.project.expensemanage.commone.utils.date.DateUtils;
 import com.project.expensemanage.domain.user.entity.User;
 import com.project.expensemanage.domain.user.enums.ServiceSubscriber;
 import com.project.expensemanage.domain.user.repository.UserRepository;
 import com.project.expensemanage.recommendation.dto.RecommendationExpenditure;
 import com.project.expensemanage.recommendation.event.event.DailyRecommendationExpenditureEvent;
 import com.project.expensemanage.recommendation.repository.RecommendationRepository;
-import java.time.LocalDate;
-import java.time.YearMonth;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +20,7 @@ public class DailyExpenditureRecommendationPublisher {
   private final UserRepository userRepository;
   private final ApplicationEventPublisher publisher;
   private final RecommendationRepository recommendationRepository;
+  private final DateUtils dateUtils;
 
   public void sendDailyRecommendation() {
     findSubscriber().forEach(this::publishEvent);
@@ -37,15 +37,6 @@ public class DailyExpenditureRecommendationPublisher {
 
   private List<RecommendationExpenditure> getTotalExpenditureMonthlyByUser(User user) {
     return recommendationRepository.findTotalExpenditureByCategoryAndDateAndId(
-        startOfMonth(), endOfMonth(), user.getId());
-  }
-
-  // 역할의 분리가 필요, 해당 역할은 따로 빼주는 것이 좋아보임 -> 추후 리팩토링 예정
-  private LocalDate startOfMonth() {
-    return YearMonth.now().atDay(1);
-  }
-
-  private LocalDate endOfMonth() {
-    return YearMonth.now().atEndOfMonth();
+        dateUtils.startOfMonth(), dateUtils.endOfMonth(), user.getId());
   }
 }
