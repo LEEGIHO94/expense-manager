c06ff6d34af3489a3436a743caaa5c7b401eb425
Siglee2247 <lgh941107@gmail.com>
2023-12-06 09:26:50 +0900

    feat: 금일 사용 예산 조회를 위한 Repository 메서드 구현 #20

--- src/main/java/com/project/expensemanage/domain/expenditure/repoistory/ExpenditureQueryDslRepository.java
+++ src/main/java/com/project/expensemanage/domain/expenditure/repoistory/ExpenditureQueryDslRepository.java
@@ -3,10 +3,11 @@ package com.project.expensemanage.domain.expenditure.repoistory;
 import com.project.expensemanage.domain.expenditure.entity.Expenditure;
 import com.project.expensemanage.domain.expenditure.repoistory.dto.GetExpenditureDetailsCondition;
 import com.project.expensemanage.domain.expenditure.repoistory.dto.TotalExpenditureByCategory;
+import java.time.LocalDate;
 import java.util.List;
-import org.springframework.data.domain.Page;
 
 public interface ExpenditureQueryDslRepository {
     List<Expenditure> findAllExpenditureByCondition(GetExpenditureDetailsCondition condition);
     List<TotalExpenditureByCategory> findTotalExpenditureByCategory(GetExpenditureDetailsCondition condition);
+    public List<TotalExpenditureByCategory> findDailyTotalExpenditureByUserId(Long userId, LocalDate date);
 }
