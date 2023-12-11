50e3415809397b4bfd59f238286fb38ba8d66927
Siglee2247 <lgh941107@gmail.com>
2023-12-11 23:11:30 +0900

    feat: 이 전달 대비 지출 비율 구현을 위한 repository 구현 #23

---
+++ src/main/java/com/project/expensemanage/domain/analysis/repository/AnalysisRepository.java
@@ -0,0 +1,45 @@
+package com.project.expensemanage.domain.analysis.repository;
+
+import static com.project.expensemanage.domain.expenditure.entity.QExpenditure.*;
+
+import com.project.expensemanage.domain.analysis.repository.dto.ExpenditureDiff;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class AnalysisRepository {
+  private final JPAQueryFactory query;
+
+  public List<ExpenditureDiff> getRateOfExpenditureDiff(Long userId, LocalDate date) {
+    return query
+        .select(
+            Projections.constructor(
+                ExpenditureDiff.class, expenditure.category.id, expenditure.category.name,
+                expenditure.price.value.sum()
+                ))
+        .from(expenditure)
+        .where(
+            userIdEq(userId),
+            expendDateRange(date)
+            )
+        .groupBy(
+            expenditure.category.id
+        )
+        .fetch();
+  }
+
+  private BooleanExpression userIdEq(Long userId) {
+    return expenditure.user.id.eq(userId);
+  }
+
+  //LocalDate를 기준으로 현재 시간과 이전 시간
+  private BooleanExpression expendDateRange(LocalDate date) {
+    return expenditure.expendedDate.between(date, date.minusMonths(1));
+  }
+}
---
+++ src/main/java/com/project/expensemanage/domain/analysis/repository/dto/ExpenditureDiff.java
@@ -0,0 +1,5 @@
+package com.project.expensemanage.domain.analysis.repository.dto;
+
+public record ExpenditureDiff(Long categoryId, String categoryName, Long rate){
+
+}
