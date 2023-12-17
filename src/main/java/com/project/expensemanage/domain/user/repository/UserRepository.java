4ffbea29da4c1bc32a25a0db6f94e95168c8d9d8
Siglee2247 <lgh941107@gmail.com>
2023-12-18 02:43:05 +0900

    style: user 하위 클래스 google lint 적용 #26

--- src/main/java/com/project/expensemanage/domain/user/repository/UserRepository.java
+++ src/main/java/com/project/expensemanage/domain/user/repository/UserRepository.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String email);
 
-    List<User> findUserByServiceSubscriber(ServiceSubscriber serviceSubscriber);
+  List<User> findUserByServiceSubscriber(ServiceSubscriber serviceSubscriber);
 }
