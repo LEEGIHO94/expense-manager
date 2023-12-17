21a4b4d78a94a9cd2fdc107a6b7daca42d0870b6
Siglee2247 <lgh941107@gmail.com>
2023-12-18 01:54:17 +0900

    test: 전체 사용자  대비 지출 controller api 단위 테스트 #23

--- src/test/java/com/project/expensemanage/domain/analysis/controller/AnalysisControllerTest.java
+++ src/test/java/com/project/expensemanage/domain/analysis/controller/AnalysisControllerTest.java
@@ -1,5 +1,6 @@
 package com.project.expensemanage.domain.analysis.controller;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,9 +8,12 @@ import com.project.expensemanage.commone.config.JacksonConfig;
 import com.project.expensemanage.commone.config.SecurityConfig;
 import com.project.expensemanage.commone.security.annotation.WithMockCustomUser;
 import com.project.expensemanage.commone.security.config.AuthTestConfig;
+import com.project.expensemanage.domain.analysis.controller.dto.ExpenditureAnalysisResponse;
 import com.project.expensemanage.domain.analysis.controller.dto.ExpenditureDiffResponse;
 import com.project.expensemanage.domain.analysis.service.AnalysisService;
 import com.project.expensemanage.domain.budget.mock.BudgetMock;
+import com.project.expensemanage.domain.expenditure.entity.Expenditure;
+import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -44,7 +48,7 @@ class AnalysisControllerTest {
   @Test
   @DisplayName("지난 주 대비 이번 주 지출 비율 테스트")
   @WithMockCustomUser
-  void get_weekly_expenditure_comparison() throws Exception{
+  void get_weekly_expenditure_comparison_test() throws Exception{
       // given
     BDDMockito.given(service.getWeeklyExpenditureComparison(Mockito.anyLong())).willReturn(expenditureDiffResponseList());
       // when
@@ -65,7 +69,7 @@ class AnalysisControllerTest {
   @Test
   @DisplayName("지난 달 대비 이번 달 지출 비율 테스트")
   @WithMockCustomUser
-  void get_monthly_expenditure_comparison() throws Exception{
+  void get_monthly_expenditure_comparison_test() throws Exception{
       // given
     BDDMockito.given(service.getMonthlyExpenditureComparison(Mockito.anyLong())).willReturn(expenditureDiffResponseList());
       // when
@@ -82,6 +86,26 @@ class AnalysisControllerTest {
         .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].categoryName").isString())
         .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].rate").isNumber());
   }
+  @Test
+  @DisplayName("전체 사용자 대비 사용자 지출 비율 테스트")
+  @WithMockCustomUser
+  void get_total_expenditure_comparison_test() throws Exception{
+      // given
+    BDDMockito.given(service.getExpenditureAnalysisByUser(Mockito.anyLong())).willReturn(expenditureAnalysisResponse());
+      // when
+    ResultActions perform = mvc.perform(MockMvcRequestBuilders.get(DEFAULT));
+    // then
+    perform
+        .andDo(MockMvcResultHandlers.log())
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data").isMap())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.timeStamp").isString())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.code").isNumber())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.message").isString())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data.analysisDate").isString())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data.expenditureRate").isNumber());
+  }
+
 
   List<ExpenditureDiffResponse> expenditureDiffResponseList(){
     return List.of(
@@ -89,5 +113,11 @@ class AnalysisControllerTest {
         ExpenditureDiffResponse.builder().categoryId(2L).categoryName("카테고리이름2").rate(20L).build(),
         ExpenditureDiffResponse.builder().categoryId(3L).categoryName("카테고리이름3").rate(30L).build()
     );
+  }
+  ExpenditureAnalysisResponse expenditureAnalysisResponse(){
+    return ExpenditureAnalysisResponse.builder()
+        .analysisDate(LocalDate.now())
+        .expenditureRate(90L)
+        .build();
 }
 }
