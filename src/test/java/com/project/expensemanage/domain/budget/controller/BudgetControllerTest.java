78e6e39fdc6e42325fc1d057a5d4c47a46b97a02
Siglee2247 <lgh941107@gmail.com>
2023-12-21 23:09:05 +0900

    docs: budget 삭제 문서화 #28

--- src/test/java/com/project/expensemanage/domain/budget/controller/BudgetControllerTest.java
+++ src/test/java/com/project/expensemanage/domain/budget/controller/BudgetControllerTest.java
@@ -10,7 +10,9 @@ import com.project.expensemanage.commone.config.SecurityConfig;
 import com.project.expensemanage.commone.security.annotation.WithMockCustomUser;
 import com.project.expensemanage.commone.security.config.AuthTestConfig;
 import com.project.expensemanage.domain.budget.mock.BudgetMock;
+import com.project.expensemanage.domain.budget.repository.BudgetRepository;
 import com.project.expensemanage.domain.budget.service.BudgetService;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
@@ -24,6 +26,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.headers.HeaderDocumentation;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.payload.PayloadDocumentation;
@@ -116,11 +119,12 @@ class BudgetControllerTest {
     BDDMockito.given(service.patchBudget(anyLong(), anyLong(), any(PatchBudgetRequest.class)))
         .willReturn(mock.idDtoMock());
     // when
-    ResultActions perform = mvc.perform(
-        MockMvcRequestBuilders.patch("/api/budgets" + "/{budgetId}", mock.getBudgetId())
-            .content(content)
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON));
+    ResultActions perform =
+        mvc.perform(
+            RestDocumentationRequestBuilders.patch("/api/budgets" + "/{budgetId}", mock.getBudgetId())
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
     // then
     perform
         .andDo(MockMvcResultHandlers.log())
@@ -131,8 +135,7 @@ class BudgetControllerTest {
                 Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                 Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
                 RequestDocumentation.pathParameters(
-                    RequestDocumentation.parameterWithName("budgetId").description("예산 식별자")
-                ),
+                    RequestDocumentation.parameterWithName("budgetId").description("예산 식별자")),
                 PayloadDocumentation.requestFields(
                     PayloadDocumentation.fieldWithPath("amount")
                         .type(JsonFieldType.NUMBER)
@@ -168,10 +171,11 @@ class BudgetControllerTest {
     // given
     BDDMockito.given(service.getBudget(anyLong())).willReturn(mock.getDtoMock());
     // when
-    ResultActions perform = mvc.perform(
-        MockMvcRequestBuilders.get("/api/budgets")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON));
+    ResultActions perform =
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/budgets")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
     // then
     perform
         .andDo(MockMvcResultHandlers.log())
@@ -197,13 +201,21 @@ class BudgetControllerTest {
                     PayloadDocumentation.fieldWithPath("data[].budgetId")
                         .type(JsonFieldType.NUMBER)
                         .description("예산 식별자"),
-                    PayloadDocumentation.fieldWithPath("data[].date").type(JsonFieldType.STRING).description("예산 날짜"),
-                    PayloadDocumentation.fieldWithPath("data[].amount").type(JsonFieldType.NUMBER).description("예산 금액"),
-                    PayloadDocumentation.fieldWithPath("data[].category").type(JsonFieldType.OBJECT).description("카테고리 데이터"),
-                    PayloadDocumentation.fieldWithPath("data[].category.categoryId").type(JsonFieldType.NUMBER).description("카테고리 식별자"),
-                    PayloadDocumentation.fieldWithPath("data[].category.categoryName").type(JsonFieldType.STRING).description("카테고리 이름")
-                )
-            ));
+                    PayloadDocumentation.fieldWithPath("data[].date")
+                        .type(JsonFieldType.STRING)
+                        .description("예산 날짜"),
+                    PayloadDocumentation.fieldWithPath("data[].amount")
+                        .type(JsonFieldType.NUMBER)
+                        .description("예산 금액"),
+                    PayloadDocumentation.fieldWithPath("data[].category")
+                        .type(JsonFieldType.OBJECT)
+                        .description("카테고리 데이터"),
+                    PayloadDocumentation.fieldWithPath("data[].category.categoryId")
+                        .type(JsonFieldType.NUMBER)
+                        .description("카테고리 식별자"),
+                    PayloadDocumentation.fieldWithPath("data[].category.categoryName")
+                        .type(JsonFieldType.STRING)
+                        .description("카테고리 이름"))));
   }
 
   @Test
@@ -213,11 +225,12 @@ class BudgetControllerTest {
     // given
     BDDMockito.given(service.getRecommendedAmountForCategory(anyLong())).willReturn(mock.getDto());
     // when
-    ResultActions perform = mvc.perform(
-        MockMvcRequestBuilders.get("/api/budgets" + "/recommendation")
-            .param("amount","100000")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON));
+    ResultActions perform =
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/budgets" + "/recommendation")
+                .param("amount", "100000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
     // then
     perform
         .andDo(MockMvcResultHandlers.log())
@@ -228,8 +241,8 @@ class BudgetControllerTest {
                 Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                 Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
                 RequestDocumentation.queryParameters(
-                    RequestDocumentation.parameterWithName("amount").description("사용자가 설정할 예산의 초액")
-                ),
+                    RequestDocumentation.parameterWithName("amount")
+                        .description("사용자가 설정할 예산의 초액")),
                 PayloadDocumentation.responseFields(
                     PayloadDocumentation.fieldWithPath("timeStamp")
                         .type(JsonFieldType.STRING)
@@ -246,11 +259,36 @@ class BudgetControllerTest {
                     PayloadDocumentation.fieldWithPath("data[].categoryId")
                         .type(JsonFieldType.NUMBER)
                         .description("카테고리 식별자"),
-                    PayloadDocumentation.fieldWithPath("data[].amount").type(JsonFieldType.NUMBER).description("카테고리 별 예산"),
-                    PayloadDocumentation.fieldWithPath("data[].categoryName").type(JsonFieldType.STRING).description("카테고리 이름")
-                )
-            ));
+                    PayloadDocumentation.fieldWithPath("data[].amount")
+                        .type(JsonFieldType.NUMBER)
+                        .description("카테고리 별 예산"),
+                    PayloadDocumentation.fieldWithPath("data[].categoryName")
+                        .type(JsonFieldType.STRING)
+                        .description("카테고리 이름"))));
   }
 
-
+  @Test
+  @WithMockCustomUser
+  @DisplayName("예산 삭제 테스트")
+  void delete_budget_success_test() throws Exception {
+    // given
+    BDDMockito.willDoNothing().given(service).deleteBudget(anyLong(), anyLong());
+    // when
+    ResultActions perform =
+        mvc.perform(
+            RestDocumentationRequestBuilders.delete("/api/budgets" + "/{budgetId}", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+    // then
+    perform
+        .andDo(MockMvcResultHandlers.log())
+        .andExpect(MockMvcResultMatchers.status().isNoContent())
+        .andDo(
+            MockMvcRestDocumentation.document(
+                "delete-budget",
+                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                RequestDocumentation.pathParameters(
+                    RequestDocumentation.parameterWithName("budgetId").description("예산 식별자"))));
+  }
 }
