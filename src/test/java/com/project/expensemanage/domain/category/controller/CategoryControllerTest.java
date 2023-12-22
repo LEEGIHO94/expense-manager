52b574b3a21fa60c24ca7d0f8ed8def2898302db
Siglee2247 <lgh941107@gmail.com>
2023-12-22 15:43:50 +0900

    docs: 단건 조회 docs 작성 #28

--- src/test/java/com/project/expensemanage/domain/category/controller/CategoryControllerTest.java
+++ src/test/java/com/project/expensemanage/domain/category/controller/CategoryControllerTest.java
@@ -1,13 +1,13 @@
 package com.project.expensemanage.domain.category.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.expensemanage.commone.config.SecurityConfig;
 import com.project.expensemanage.commone.security.annotation.WithMockCustomUser;
 import com.project.expensemanage.commone.security.config.AuthTestConfig;
 import com.project.expensemanage.domain.category.dto.request.PostStandardCategoryRequest;
-import com.project.expensemanage.domain.category.mapper.CategoryMapper;
 import com.project.expensemanage.domain.category.mock.CategoryMock;
 import com.project.expensemanage.domain.category.service.CategoryService;
 import org.junit.jupiter.api.DisplayName;
@@ -22,9 +22,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.headers.HeaderDocumentation;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -36,8 +38,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
   SecurityConfig.class,
   AuthTestConfig.class,
   CategoryService.class,
-  CategoryMock.class,
-  CategoryMapper.class
+  CategoryMock.class
 })
 @AutoConfigureRestDocs
 class CategoryControllerTest {
@@ -104,7 +105,7 @@ class CategoryControllerTest {
   }
 
   @Test
-  @DisplayName("카테고리 조회 : 성공")
+  @DisplayName("카테고리 다건 조회 : 성공")
   @WithMockCustomUser
   void get_category_list_test() throws Exception {
     // given
@@ -140,6 +141,51 @@ class CategoryControllerTest {
                         .type(JsonFieldType.NUMBER)
                         .description("카테고리 식별자"),
                     PayloadDocumentation.fieldWithPath("data[].name")
+                        .type(JsonFieldType.STRING)
+                        .description("카테고리 이름"))));
+  }
+
+  @Test
+  @DisplayName("카테고리 단건 조회 : 성공")
+  @WithMockCustomUser
+  void get_category_test() throws Exception {
+    // given
+    BDDMockito.given(service.getCategory(anyLong()))
+        .willReturn(mock.getCategoryResponseList().get(1));
+    // when
+    ResultActions perform = mvc.perform(
+        RestDocumentationRequestBuilders.get("/api/categories" + "/{categoryId}",1L));
+    // then
+    perform
+        .andDo(MockMvcResultHandlers.log())
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data").isMap())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data.categoryId").isNumber())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.data.name").isString())
+        .andDo(
+            MockMvcRestDocumentation.document(
+                "get-category",
+                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                RequestDocumentation.pathParameters(
+                    RequestDocumentation.parameterWithName("categoryId").description("카테고리 식별자")),
+                PayloadDocumentation.responseFields(
+                    PayloadDocumentation.fieldWithPath("timeStamp")
+                        .type(JsonFieldType.STRING)
+                        .description("전송 시간"),
+                    PayloadDocumentation.fieldWithPath("code")
+                        .type(JsonFieldType.NUMBER)
+                        .description("상태 코드"),
+                    PayloadDocumentation.fieldWithPath("message")
+                        .type(JsonFieldType.STRING)
+                        .description("상태 메시지"),
+                    PayloadDocumentation.fieldWithPath("data")
+                        .type(JsonFieldType.OBJECT)
+                        .description("전송 데이터"),
+                    PayloadDocumentation.fieldWithPath("data.categoryId")
+                        .type(JsonFieldType.NUMBER)
+                        .description("카테고리 식별자"),
+                    PayloadDocumentation.fieldWithPath("data.name")
                         .type(JsonFieldType.STRING)
                         .description("카테고리 이름"))));
   }
