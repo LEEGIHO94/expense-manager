78df924107899b702af2ab38d4e726b992567d68
Siglee2247 <lgh941107@gmail.com>
2024-03-04 23:42:13 +0900

    fit: redis Cache Config 직렬화 정보 추가 - new Jackson2JsonRedisSerializer<>(Object.class) 사용 이유     - List<?>와 같은 데이터도 사용하기 위해

--- src/main/java/com/project/expensemanage/commone/config/CacheConfig.java
+++ src/main/java/com/project/expensemanage/commone/config/CacheConfig.java
@@ -1,5 +1,7 @@
 package com.project.expensemanage.commone.config;
 
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
+
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +13,8 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter.TtlFunction;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
@@ -27,6 +31,8 @@ public class CacheConfig {
 
   private RedisCacheConfiguration defaultConfig() {
     return RedisCacheConfiguration.defaultCacheConfig()
+        .serializeKeysWith(fromSerializer(new StringRedisSerializer()))
+        .serializeValuesWith(fromSerializer(new Jackson2JsonRedisSerializer<>(Object.class)))
         .disableCachingNullValues()
         .enableTimeToIdle()
         .entryTtl(new FixedDurationTtlFunction(Duration.ofHours(1)));
