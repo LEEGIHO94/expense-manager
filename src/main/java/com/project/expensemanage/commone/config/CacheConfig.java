b9fac07e5374101f239e48cc36a5956b2c7bbd4a
Siglee2247 <lgh941107@gmail.com>
2024-03-04 22:39:10 +0900

    feat: redis Cache Config 설정 - Tti 적용 - Category 캐시 만료 시간 없음

--- src/main/java/com/project/expensemanage/commone/config/CacheConfig.java
+++ src/main/java/com/project/expensemanage/commone/config/CacheConfig.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.FixedDurationTtlFunction;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter.TtlFunction;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
@@ -16,23 +18,25 @@ public class CacheConfig {
 
   @Bean
   public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-    return RedisCacheManager.builder()
+    return RedisCacheManager.builder(connectionFactory)
         .cacheDefaults(defaultConfig())
         .withInitialCacheConfigurations(customConfigMap())
+        .transactionAware()
         .build();
   }
 
   private RedisCacheConfiguration defaultConfig() {
     return RedisCacheConfiguration.defaultCacheConfig()
-//        .serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
-//        .serializeValuesWith(
-//            SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-        .entryTtl(Duration.ofMillis(1));
+        .disableCachingNullValues()
+        .enableTimeToIdle()
+        .entryTtl(new FixedDurationTtlFunction(Duration.ofHours(1)));
   }
 
   private Map<String, RedisCacheConfiguration> customConfigMap() {
     Map<String, RedisCacheConfiguration> cacheConfigMap = new HashMap<>();
-    //cache 항목에 따른 기간 설정을 위한 Config
+
+    cacheConfigMap.put("CATEGORY",defaultConfig().entryTtl(TtlFunction.persistent()));
+
     return cacheConfigMap;
   }
 }
