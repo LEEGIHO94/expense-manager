888bc2451f1c54cfcaf99767487e95b98621ddb9
Siglee2247 <lgh941107@gmail.com>
2024-03-28 01:42:26 +0900

    deploy: build 파일 2개 추가 1. build 이후 docker-compose 처리 2. build 이후 일반 실행

---
+++ script/buildNDocker.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+./gradlew clean build
+
+if [ $? -eq 0 ]; then
+    docker-compose up --build
+else
+    echo "Build failed!! Aborting docker-compose up --build."
+    echo "check your resource"
+fi
+
