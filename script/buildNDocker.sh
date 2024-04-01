b04bc16084016ea941ae67a669c099d2fab4f78f
Siglee2247 <lgh941107@gmail.com>
2024-04-02 00:06:36 +0900

    deploy: compose -d옵션 추가

--- script/buildNDocker.sh
+++ script/buildNDocker.sh
@@ -4,7 +4,7 @@
 ./gradlew clean build
 
 if [ $? -eq 0 ]; then
-    docker-compose up --build
+    docker-compose up --build -d
 else
     echo "Build failed!! Aborting docker-compose up --build."
     echo "check your resource"
